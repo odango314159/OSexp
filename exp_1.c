6eb337b33645851c2f8170631df9d8655625faa9
ub220034 <ub220034@remote09.educ.cc.keio.ac.jp>
2023-11-08 17:19:50 +0900

    first

---
+++ exp_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int a;
+static int b;
+int c =100;
+static int d=200;
+
+
+int main(){
+    char *p,*q;
+
+    p = malloc(1024);
+    q = malloc(512);
+
+    printf("a is at %p\n",&a);
+    printf("b is at %p\n",&b);
+    printf("c is at %p\n",&c);
+    printf("d is at %p\n",&d);
+    printf("p is at %p\n",p);
+    printf("q is at %p\n",q);
+    return 0;
+}
